98201528b5a00baf051c3bfd85f083efe7571210
amitwidhani <31656045+amitwidhani@users.noreply.github.com>
2017-10-19 12:55:52 +0100

    Zipkin

--- src/main/java/com/ba/AccountApplication.java
+++ src/main/java/com/ba/AccountApplication.java
@@ -2,6 +2,8 @@ package com.ba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccountApplication {
@@ -9,4 +11,9 @@ public class AccountApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
 	}
+	
+	@Bean
+	public AlwaysSampler defaultSampler() {
+	  return new AlwaysSampler();
+	}	
 }
